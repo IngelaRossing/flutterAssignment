aa5107eaf38225b4ffdcefc5e3ddc4f6d07ddb8f
Ingela Rossing <ingela.rossing@ait.gu.se>
2020-10-26 17:09:47 +0100

    Start of scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'My app',
+        theme: ThemeData(
+          primaryColor: Colors.grey,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('TIG169 TODO'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            //onPressed:
+            child: const Icon(Icons.add),
+          ),
+        ));
+  }
+}
