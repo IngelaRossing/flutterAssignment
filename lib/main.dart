69847ffbc01887d16666e2a56b25a4ac0c9cf0cb
Ingela Rossing <pringela@gmail.com>
2020-10-27 14:25:13 +0100

    Added a ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,54 @@
 import 'package:flutter/material.dart';
-
+  
 void main() {
-  runApp(MyApp());
+  runApp(TodoApp());
 }
 
-class MyApp extends StatelessWidget {
+// A page where we display all of our items
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'My app',
-        theme: ThemeData(
-          primaryColor: Colors.grey,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('TIG169 TODO'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            //onPressed:
-            child: const Icon(Icons.add),
-          ),
-        ));
+    return new MaterialApp(
+      title: 'Todo List',
+      theme: ThemeData(
+        primaryColor: Colors.grey,
+      ),
+      // A view of list that can change
+      home: TodoList() 
+    );
   }
 }
+
+class TodoList extends StatefulWidget {
+  @override
+  createState() => TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+
+  List<String> todos = ['hi', 'oh', 'there', 'I', 'didnt'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TIG169 TODO'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        //onPressed: go to AddItemPage
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder( 
+        itemCount: todos.length, 
+        itemBuilder: (BuildContext context,int index){ 
+          return ListTile( 
+            leading: Icon(Icons.list), 
+            title: Text("${todos[index]}") ,
+            trailing: Icon(Icons.list),
+          ); 
+        } 
+      ),
+    );
+  }
+}
+
